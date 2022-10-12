5d5db69799c13e14059bee4cb51dbcbb930d1bba
Angela Carvalho <angelalopo@outlook.com>
2022-10-12 12:26:19 -0300

    insere configuracoes postcss tailwind e storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
+import './styles/global.css'; 
 
 export function App() {
 
   return (
-    <div className="App">
-      
+    <div className="font-bold text-5xl text-violet-500">
+      <h1>Hello world</h1>
     </div>
   )
 }
